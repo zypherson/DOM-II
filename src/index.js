083829498008bbbee9b100b4e8bdbc7ecffdb110
zypherson <taylorzypherson@gmail.com>
2022-02-08 21:15:55 -0500

    completed dom-II

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 import './less/index.less'
 
 // Your code goes here!
+// load
+window.onload = function(evt){
+    console.log(`event ${evt.type} fired.Ready to go!`)
+    const heading = document.querySelector('h1')
+    heading.textContent = "ready to go"
+
+  //2-copy 
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+        .then(text =>{
+            console.log(text)
+        })
+       
+    })
+
+    // click
+    document.body.addEventListener('click', evt => {
+        evt.target.classList.toggle('mirror')
+    })
+
+    //double click
+    document.body.addEventListener('dblclick', evt => {
+        evt.target.innerHTML = ''
+    })
+    // keydown
+    window.addEventListener('keydown', evt =>{
+        if(evt.key == 6){
+            document.body.innerHTML = ''
+        }
+    })
+    // mousemove
+    document.body.addEventListener('mousemove', evt =>{
+        const { clientX, clientY} = evt
+    // console.log(`mouse is at ${clientX},${clientY}!)
+    })
+
+    // mouseenter
+    // mouseleave
+    const destinations = document.querySelectorAll('.destination')
+    for(let destination of destinations){
+        destination.addEventListener('mouseenter', () =>{
+            destination.style.fontWeight = 'bold'
+
+        })
+        destination.addEventListener('mouseleave', () =>{
+            destination.style.fontWeight = 'initial'
+        })
+
+    }
+
+
+
+}
+
